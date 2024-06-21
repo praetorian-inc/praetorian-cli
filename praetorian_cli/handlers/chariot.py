560f30f22d911ae1ce2c16b3622a5a24efa0f42c
praetorian-rad <rad.kawar@praetorian.com>
2024-06-21 21:50:44 +0100

    search .praetorian for scripts

--- praetorian_cli/handlers/chariot.py
+++ praetorian_cli/handlers/chariot.py
@@ -14,7 +14,7 @@ def chariot(ctx):
 
 def load_cli_scripts():
     plugins = [load_raw_script(os.path.join('scripts/', filename))
-               for filename in os.listdir('scripts/') if filename.endswith('.py')]
+               for filename in os.listdir('scripts/')+ os.listdir(os.path.expanduser('~/.praetorian/')) if filename.endswith('.py')]
     for plugin in filter(lambda p: hasattr(p, 'register') and callable(p.register), plugins):
         plugin.register(chariot)
 
