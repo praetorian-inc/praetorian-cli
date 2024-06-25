b6b9acc42656ede6405ecb8d10057e75381b7300
Ayushi Asthana <ayushi.asthana@praetorian.com>
2024-06-26 03:26:07 +0530

    DocString to clarify that it is a development feature.

--- praetorian_cli/scripts/hello.py
+++ praetorian_cli/scripts/hello.py
@@ -1,7 +1,15 @@
 import click
 
+""" 
+For developers: 
+You can use this as a template for testing new commands/scripts.
+It is hidden from the help menu, but can still be called from the command line. 
+Usage : 
+    praetorian chariot hello <name>
+"""
 
-@click.command()
+
+@click.command(hidden=True)
 @click.pass_context
 @click.argument('name', type=str, required=True)
 def hello(ctx: click.Context, name: str):
