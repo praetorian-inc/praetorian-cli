3997d2f6a589092c744ff9606c5e5f7c81e49b24
Ayushi Asthana <ayushi.asthana@praetorian.com>
2024-06-25 13:58:31 +0530

    Rename command to run

--- praetorian_cli/handlers/plugin.py
+++ praetorian_cli/handlers/plugin.py
@@ -2,21 +2,21 @@ import click
 
 from praetorian_cli.handlers.chariot import chariot
 from praetorian_cli.handlers.cli_decorators import cli_handler
-from praetorian_cli.scripts import hello_plugin
+from praetorian_cli.scripts import hello_run
 
 
 @chariot.group()
 @cli_handler
-def plugin(controller):
-    """Run a plugin with arguments"""
+def run(controller):
+    """Run a script with arguments"""
     pass
 
 
-@plugin.command('hello')
+@run.command('hello')
 @click.argument('args', nargs=-1)
 @click.option('--kwargs', '-k', multiple=True, type=(str, str), help="Key-value pairs for the plugin")
 @click.option('--strings', '-s', multiple=True, help="Multiple strings")
 @cli_handler
 def hello(controller, args, kwargs, strings):
     """Run the hello plugin"""
-    hello_plugin.hello_function(controller, args, kwargs, strings)
+    hello_run.hello_function(controller, args, kwargs, strings)
