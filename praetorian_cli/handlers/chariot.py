ba7b3f2d26f5ee5bd3a5ce70f3dea0040e7df13b
Peter Kwan <peter.kwan@praetorian.com>
2024-06-26 20:33:23 -0700

    removed dynamically load from the 'chariot' group

--- praetorian_cli/handlers/chariot.py
+++ praetorian_cli/handlers/chariot.py
@@ -11,16 +11,3 @@ from praetorian_cli.sdk.chariot import Chariot
 def chariot(ctx):
     """ Chariot API access in the new and different file """
     ctx.obj = Chariot(keychain=ctx.obj)
-
-
-def load_cli_scripts():
-    module_dir = os.path.dirname(
-        importlib.util.find_spec('praetorian_cli').origin)
-    scripts_dir = os.path.join(module_dir, 'scripts')
-    plugins = [load_raw_script(os.path.join(scripts_dir, filename))
-               for filename in os.listdir(scripts_dir) if filename.endswith('.py')]
-    for plugin in filter(lambda p: hasattr(p, 'register') and callable(p.register), plugins):
-        plugin.register(chariot)
-
-
-load_cli_scripts()
