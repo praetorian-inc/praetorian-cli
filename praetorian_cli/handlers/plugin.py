c4c1f31f5722c52a05cfb45278873448df8922fc
Peter Kwan <peter.kwan@praetorian.com>
2024-06-26 17:05:11 -0700

    Renamed the 'run' command group to 'plugin'

--- praetorian_cli/handlers/plugin.py
+++ praetorian_cli/handlers/plugin.py
@@ -7,20 +7,20 @@ from praetorian_cli.scripts import hello_command
 
 @chariot.group()
 @cli_handler
-def run(controller):
-    """Run a script with arguments"""
+def plugin(controller):
+    """ Run a plugin command """
     pass
 
 
-@run.command('hello')
+@plugin.command('hello')
+@cli_handler
 @click.argument('arg1', type=str)
 @click.argument('arg2', type=int)
-@click.option('--opt1', default=None, help="A string option")
+@click.option('--opt1', default=None, help='A string option')
 @click.option('--sow', required=True,
-              help="A mandatory option to provide the SOW number; will prompt if not supplied",
+              help='A mandatory option to provide the SOW number; will prompt if not supplied',
               prompt='SOW number is required. What is the SOW number?')
 @click.option('--flag-opt', is_flag=True, help='A flag option')
-@cli_handler
 def hello(controller, arg1, arg2, opt1, sow, flag_opt):
-    """Run the hello plugin"""
+    """ Example plugin command, extending the core list of commands """
     hello_command.hello_function(controller, arg1, arg2, opt1, sow, flag_opt)
