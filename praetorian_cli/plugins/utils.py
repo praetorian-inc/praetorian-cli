974ec524ff7dafc32815fd9e315aa96a54a3fc4a
Peter Kwan <peter.kwan@praetorian.com>
2024-07-02 20:28:57 -0700

    added support for package prerequisite

--- praetorian_cli/plugins/utils.py
+++ praetorian_cli/plugins/utils.py
@@ -1,20 +1,59 @@
+from importlib import import_module
 from shutil import which
-from sys import stderr
 from functools import wraps
 
+from click import echo
+from semver import compare
 
-def requires(command, help=None):
+
+def requires_command(command, help=None):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             if which(command) is not None:
                 return func(*args, **kwargs)
             if help:
-                print(help, file=stderr)
+                exit_with_message(help)
             else:
-                print(f"This function requires {command} to be installed.", file=stderr)
-            exit(1)
+                exit_with_message(f'This plugin requires the {command} command. Please install it.')
 
         return wrapper
 
     return decorator
+
+
+def requires_package(package, version=None, help=None):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+
+            def not_satisfied():
+                if help:
+                    exit_with_message(help)
+                else:
+                    if version:
+                        exit_with_message(
+                            f'This plugin requires the {package} ({version}) package. Please install it with pip.')
+                    else:
+                        exit_with_message(
+                            f'This plugin requires the {package} package. Please install it with pip.')
+
+            try:
+                module = import_module(package)
+            except:
+                not_satisfied()
+
+            if version and compare(module.__version__, version) == -1:
+                not_satisfied()
+
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+
+def exit_with_message(message):
+    echo(message, err=True)
+    exit(1)
