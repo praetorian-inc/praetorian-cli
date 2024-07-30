4a2d1d7c8bdb88414ab76a8b339c38d09a009e38
Peter Kwan <peter.kwan@praetorian.com>
2024-07-29 21:50:32 -0700

    print the date of the backend hash

--- praetorian_cli/handlers/version.py
+++ praetorian_cli/handlers/version.py
@@ -1,3 +1,5 @@
+import datetime
+
 import click
 
 from praetorian_cli.handlers.chariot import chariot
@@ -8,5 +10,5 @@ from praetorian_cli.handlers.cli_decorators import cli_handler
 @cli_handler
 def version(controller):
     """ Retrieve version information of Chariot """
-    version = controller.version()
-    click.echo(f'Chariot backend version: {version['backend_hash']} ()')
+    v = controller.version()
+    click.echo(f'Chariot backend version: {v["backend_hash"]} ({datetime.date.fromtimestamp(v["backend_date"])})')
