fc5a475cad9cfdca18a627680ca006a4d61ed0dc
Peter Kwan <peter.kwan@praetorian.com>
2024-07-29 22:23:47 -0700

    removed date; simplified things

--- praetorian_cli/handlers/version.py
+++ praetorian_cli/handlers/version.py
@@ -1,5 +1,3 @@
-import datetime
-
 import click
 
 from praetorian_cli.handlers.chariot import chariot
@@ -10,5 +8,4 @@ from praetorian_cli.handlers.cli_decorators import cli_handler
 @cli_handler
 def version(controller):
     """ Retrieve version information of Chariot """
-    v = controller.version()
-    click.echo(f'Chariot backend version: {v["backend_hash"]} ({datetime.date.fromtimestamp(v["backend_date"])})')
+    click.echo(f'Chariot backend version: {controller.version()["version"]}')
