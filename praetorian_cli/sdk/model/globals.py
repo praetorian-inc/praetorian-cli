a7bd66c4584731e1596e9b5388986fff526af3a6
Peter Kwan <peter.kwan@praetorian.com>
2024-12-30 11:43:55 -0800

    updated to use the status strings from Asset

--- praetorian_cli/sdk/model/globals.py
+++ praetorian_cli/sdk/model/globals.py
@@ -28,11 +28,11 @@ class Seed(Enum):
 
 
 class Attribute(Enum):
-    ACTIVE = 'A'
-    ACTIVE_LOW = 'AL'
-    PENDING = 'P'
-    PENDING_LOW = 'PL'
-    DELETED = 'D'
+    ACTIVE = Asset.ACTIVE.value
+    ACTIVE_LOW = Asset.ACTIVE_LOW.value
+    PENDING = Asset.PENDING.value
+    PENDING_LOW = Asset.PENDING_LOW.value
+    DELETED = Asset.DELETED.value
 
 
 class Risk(Enum):
