9b4825e4edc416702929735ce766db294f76e3fc
DS-Koolaid <matthew.jackoski@praetorian.com>
2026-01-12 15:24:18 -0800

    feat(recording): enable SessionRecorder and PTYHandler exports
    - Uncomment SessionRecorder and PTYHandler imports in __init__.py
    - Both classes are now fully implemented with passing tests
    - SessionRecorder orchestrates PTYHandler and AsciinemaWriter
    - All 3 tests passing in test_session_recorder.py

--- praetorian_cli/ui/aegis/recording/__init__.py
+++ praetorian_cli/ui/aegis/recording/__init__.py
@@ -1,7 +1,6 @@
 """Session recording for aegis SSH connections."""
-# TODO: Uncomment when implementing these classes in later tasks
-# from .session_recorder import SessionRecorder
-# from .pty_handler import PTYHandler
+from .session_recorder import SessionRecorder
+from .pty_handler import PTYHandler
 from .asciinema_writer import AsciinemaWriter
 
-__all__ = ['AsciinemaWriter']  # Will add SessionRecorder and PTYHandler later
+__all__ = ['SessionRecorder', 'PTYHandler', 'AsciinemaWriter']
